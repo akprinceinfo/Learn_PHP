e41f755b5b7957e8e0a5df7334536f8881159141
prince954 <akprince0268@gmail.com>
2023-09-06 01:44:23 +0600

    PHP : Operator

---
+++ Operator/Operator.php
@@ -0,0 +1,51 @@
+
+
+<!-- ==============Operator================ -->
+
+
+
+
+
+<?php
+
+    $firstNumber = 80 ;
+    $lastNumber = 50;
+
+// ============== Assignment Operator ================ 
+
+    // echo ($firstNumber + $lastNumber) . "<br/>";
+    // echo ($firstNumber - $lastNumber) . "<br/>";
+    // echo ($firstNumber * $lastNumber) . "<br/>";
+    // echo ($firstNumber / $lastNumber) . "<br/>";
+
+    // ============== Assignment Operator ================ 
+
+    //echo ( $firstNumber += $lastNumber)."<br/>";//$firstNumber = $firstNumber + $lastNumber
+    //echo ( $firstNumber -= $lastNumber)."<br/>"; //$firstNumber = $firstNumber - $lastNumber
+    //echo ( $firstNumber *= $lastNumber)."<br/>"; //$firstNumber = $firstNumber *$lastNumber
+    //echo ( $firstNumber /= $lastNumber)."<br/>";//$firstNumber = $firstNumber / $lastNumber
+
+    // ============== Comparison Operator ================ 
+    
+    //var_dump($firstNumber == $lastNumber); //bool(false) 
+    //var_dump($firstNumber === $lastNumber); //bool(false) 
+    
+
+
+    // ============== Logical Operator  ================ 
+
+    $a = 5 ;
+    $b = 10 ;
+
+    var_dump($a == 10 && $b==10 ); //bool(false) 
+    var_dump($a == 10 || $b==10 ); //bool(false) 
+
+
+
+?>
+
+
+
+
+
+
