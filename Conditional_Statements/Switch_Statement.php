e75a2375cfd40c31b3b331f512f72060107b4338
prince954 <akprince0268@gmail.com>
2023-09-07 00:44:54 +0600

    PHP : Conditional Statements

---
+++ Conditional_Statements/Switch_Statement.php
@@ -0,0 +1,29 @@
+<?php
+
+    $color = "green";
+
+    switch($color){
+        case "red" : 
+            echo "This Color is Red";
+            break;
+
+        case "green" :
+            echo "This Color is Red";
+            break;
+
+        case "green":
+            echo "This Color is Green";
+            break;
+
+        default:
+            echo "This Color is Not ";
+    }
+
+?>
+
+
+
+
+
+
+
---
+++ Conditional_Statements/if_else.php
@@ -0,0 +1,22 @@
+
+<!-- =============Conditional Statements=================== -->
+
+<?php
+
+    $a = 10;
+    $b = 10;
+
+    if($a > $b){
+        echo "Hight Number $a";
+    }elseif($a < $b){
+        echo "This Is loar Number $b";
+    }elseif($a == $b ){
+        echo "This Number Is same $a";
+    }else{
+        echo "Not Fount";
+    }
+
+
+
+
+?>
