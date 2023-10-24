c03d97f4181ec84228d920179d5474876092842f
prince954 <akprince0268@gmail.com>
2023-10-25 00:38:18 +0600

    array marge and compine Done

--- 11.Array/array_marge_and_compine.php
+++ 11.Array/array_marge_and_compine.php
@@ -20,27 +20,41 @@
     $array3 = array("F-Number" => 50, "S-Number" =>20, "L-Number" =>30);
     $array4 = array("F-Number" => 50, "Se-Number" =>20, "L-Number" =>30);
     $result = array_merge( $array1, $array2, $array3 );
+
+
+    // আবার জখন Array ইনডেক্স এর মান যদি একই হয় তখন array_marge_recursive ব্যবহার করলে সকল মান পাওয়া যায় ।
     $result = array_merge_recursive( $array1, $array2, $array3,$array4 );
 
     echo "<pre>";
         print_r($result);
     echo "</pre>";
 
-    // আবার জখন Array ইনডেক্স এর মান যদি একই হয় তখন array_marge_recursive ব্যবহার করলে সকল মান পাওয়া যায় ।
+    
 
 
     //----------OutPut-------------
     //  Array
     // (
-    //     [0] => Apple
-    //     [1] => banana
-    //     [2] => Coconut
-    //     [3] => grape
-    //     [4] => orange
-    //     [5] => banana
-    //     [F-Number] => 50
-    //     [S-Number] => 20
-    //     [L-Number] => 30
+    //         [0] => Apple
+            // [1] => banana
+            // [2] => Coconut
+            // [3] => grape
+            // [4] => orange
+            // [5] => banana
+            // [F-Number] => Array
+            //     (
+            //         [0] => 50
+            //         [1] => 50
+            //     )
+
+            // [S-Number] => 20
+            // [L-Number] => Array
+            //     (
+            //         [0] => 30
+            //         [1] => 30
+            //     )
+
+            // [Se-Number] => 20
     // )
 
 ?>
@@ -53,10 +67,13 @@
 
 <?php
 
-    // $keys = array("a", "b", "c");
-    // $values = array("apple", "banana", "cherry");
-    // $result = array_combine($keys, $values);
-    // print_r($result);
+    $keys = array("a", "b", "c");
+    $values = array("apple", "banana", "cherry");
+    $result = array_combine($keys, $values);
+    echo "<pre>";
+        print_r($result);
+    echo "</pre>";
+
 
 
 
