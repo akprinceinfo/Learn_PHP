74b6463287ed3461a846fef3f4eb9775dff64f82
prince954 <akprince0268@gmail.com>
2024-01-05 23:45:49 +0600

    Contact_Form

---
+++ 16.Contact_Form/ContactForm.php
@@ -0,0 +1,106 @@
+
+      <!-- =============== Php use================= -->
+         <?php 
+         
+            if(isset($_POST['submit'])){
+               $fname = $_POST['fname'];
+               $lname = $_POST['lname'];
+               $email = $_POST['email'];
+               $birthday = $_POST['birthday'];
+
+               $error = [];
+
+               if(empty($fname)){
+                  $error['fname'] = "Please, Insert Your fname.";
+               };
+               if(empty($fname)){
+                  $error['lname'] = "Please, Insert Your lname.";
+               };
+               if(empty($fname)){
+                  $error['email'] = "Please, Insert Your email.";
+               };
+               if(empty($fname)){
+                  $error['birthday'] = "Please, Insert Your birthday.";
+               };
+
+            }
+         
+         ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      <link rel ="stylesheet" href="style.css">
+   </head>
+   <body>
+      <div class="container">
+         <form action="" method="POST">
+          <fieldset>
+              <legend> Personalia: </legend>
+              <label for="fname">First name:</label>
+              <input type="text" id="fname" name="fname" value="<?php 
+                  if(isset($fname)) { echo $fname;}
+              ?>"  >
+
+              <span style ="color:red">
+               <?php 
+                if(isset($error['fname'])){
+                  echo $error['fname'];
+                }
+              ?>
+              </span>
+               <br><br>
+              <label for="lname">Last name:</label>
+              <input type="text" id="lname" name="lname" value="<?php 
+                  if(isset($fname)) { echo $fname;}
+              ?>">
+                <span style ="color:red">
+               <?php 
+                if(isset($error['lname'])){
+                  echo $error['lname'];
+                }
+              ?>
+              </span>
+              <br><br>
+              <label for="email">Email:</label>
+              <input type="email" id="email" name="email" value="<?php 
+                  if(isset($email)) { echo $email;}
+              ?>">
+                <span style ="color:red">
+               <?php 
+                if(isset($error['email'])){
+                  echo $error['email'];
+                }
+              ?>
+              </span>
+              <br><br>
+              <label for="birthday">Birthday:</label>
+              <input type="date" id="birthday" name="birthday" value="<?php 
+                  if(isset($birthday)) { echo $birthday;}
+              ?>">
+                <span style ="color:red">
+               <?php 
+                if(isset($error['birthday'])){
+                  echo $error['birthday'];
+                }
+              ?>
+              </span>
+              <br><br>
+              <label for="cars">Choose a car:</label>
+              <select id="cars">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select><br><br>
+              <input class="btn" type="submit" value="Submit" name="submit">
+            </fieldset>
+        </form>
+      </div>
+   </body>
+</html>
