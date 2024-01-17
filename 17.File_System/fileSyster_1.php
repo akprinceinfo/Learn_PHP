915babf29a91c23b90a7486750f385c7d45d7cff
prince954 <akprince0268@gmail.com>
2024-01-18 00:21:17 +0600

    Php File Opration

---
+++ 17.File_System/fileSyster_1.php
@@ -0,0 +1,18 @@
+
+<?php 
+
+    $file = 'hello.txt';
+
+    //echo readfile($file); // File Read Kora
+    //mkdir('assect');   // New Folder Creater
+    //rmdir('assect');  // Folder Delete
+    //echo filetype ('assect'); // File Type Checker
+    //echo realpath($file);
+
+
+
+
+
+
+
+?>
