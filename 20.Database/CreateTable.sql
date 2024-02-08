6d3788445eb13a2cf4f0cae8c3f62eb4fede849d
prince954 <akprince0268@gmail.com>
2024-02-09 00:10:34 +0600

    CreateTable

---
+++ 20.Database/CreateTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE tableName(
+    if INT(8) NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    class VARCHAR(16) NOT NULL,
+    roll INT(3), UNIQUE,
+    admission_date DATE,
+    PRIMARY KEY(id)
+
+
+) ;
