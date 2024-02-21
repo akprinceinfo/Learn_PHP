e39d4a0d7857143e0be2c81ea3bb46a7503ea4dd
prince954 <akprince0268@gmail.com>
2024-02-22 01:49:40 +0600

    UpdateData

---
+++ 20.Database/UpdateData.sql
@@ -0,0 +1,13 @@
+-- update table name change
+
+UPDATE 'table name' SET name = "new table name" WHERE id = 2;
+
+-- full table row change
+
+UPDATE 'tableName'
+SET
+    name = 'Your name',
+    id = '',
+    roll = ''
+WHERE
+    id = '4';
